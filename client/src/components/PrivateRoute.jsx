8419cf3adf8f967416b28b3cd46f89f782c710aa
mustaqinIshak <mustaqinishak@gmail.com>
2021-09-24 17:12:47 +0800

    first commit

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+'use strict'
+
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import {Route, Redirect} from 'react-router-dom'
+
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render= {props => (
+        localStorage.getItem('auth') ? <Component {...props} /> : <Redirect to={{pathname: '/', state: { from: props.location }}} />
+    )} />
+)
---
+++ client/src/components/home/home.componet.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React, {Component} from 'react'
+import PropsTypes from  'prop-types'
+import {withStyler} from '@material-ui/core/styles'
+import {connect} from 'react-redux'
+import { withRouter } from 'react-router' 
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    }
+})
