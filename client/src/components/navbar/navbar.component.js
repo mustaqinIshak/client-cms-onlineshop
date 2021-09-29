20703190ebdfe4207d49c260913ea15eb3ebe16c
mustaqinIshak <mustaqinishak@gmail.com>
2021-09-29 13:13:32 +0800

    navbar component

---
+++ client/src/components/navbar/navbar.component.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+import {connect} from 'react-redux'
+import {history} from '../../helpers'
+
+class Navbar extends Component {
+    consturctor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div className="navbar">
+                
+            </div>
+        )
+    }
+
+} 
